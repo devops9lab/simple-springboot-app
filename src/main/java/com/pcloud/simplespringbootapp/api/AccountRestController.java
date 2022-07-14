de56fdb4c00ba354e1f44fd507ab2695c0a58df9
The Tran <tran.q.the@gmail.com>
2022-07-14 14:25:00 +0700

    rate limiter events

--- src/main/java/com/pcloud/simplespringbootapp/api/AccountRestController.java
+++ src/main/java/com/pcloud/simplespringbootapp/api/AccountRestController.java
@@ -2,7 +2,9 @@ package com.pcloud.simplespringbootapp.api;
 
 import com.pcloud.simplespringbootapp.db.Account;
 import com.pcloud.simplespringbootapp.db.AccountRepository;
+import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import javax.annotation.PostConstruct;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountRestController {
+
+    private final RateLimiterRegistry rateLimiterRegistry;
     private final AccountRepository accountRepository;
 
-    public AccountRestController(AccountRepository accountRepository) {
+    public AccountRestController(AccountRepository accountRepository, RateLimiterRegistry rateLimiterRegistry) {
         this.accountRepository = accountRepository;
+        this.rateLimiterRegistry = rateLimiterRegistry;
     }
+
+    @PostConstruct
+    public void postConstruct() {
+        io.github.resilience4j.ratelimiter.RateLimiter.EventPublisher eventPublisher = rateLimiterRegistry
+            .rateLimiter("basic")
+            .getEventPublisher();
+
+        eventPublisher.onSuccess(System.out::println);
+        eventPublisher.onFailure(System.out::println);
+    }
+
 
     @RateLimiter(name = "basic")
     @GetMapping
